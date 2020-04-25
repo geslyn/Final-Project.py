2505a6bb5d9d125ef389a0ed81f1c1e52409cbda
geslyn <61592906+geslyn@users.noreply.github.com>
2020-04-25 18:50:44 -0400

    Update Test 1.py

--- Test 1.py
+++ Test 1.py
@@ -1,21 +1,21 @@
 import random
 import os
 
-name = input('What is your name? \n')
+name = input('Hello! What is your name? \n')
 
 randomNumber = random.randint(1, 50)
 
-guess = int (input("Welcome, %s , guess a number between 1 and 50: " % (name)))
+guess = int (input("Welcome, %s! , Lets play a game. Please guess a number between 1 and 50: " % (name)))
 
 while randomNumber != guess:
 
  if guess < randomNumber:
      print ("Hint: Guess is too low! Try again.")
-     guess = int(input("enter a number between 1 and 50: "))
+     guess = int(input("Guess a number between 1 and 50: "))
     
  elif guess > randomNumber:
      print ("Hint: Guess is too high! Try again.")
-     guess = int(input("enter a number between 1 and 50: "))
+     guess = int(input("Guess a number between 1 and 50: "))
 
 print('Great Job! You guessed it')
 os.system("pause")
