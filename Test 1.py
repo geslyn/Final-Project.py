3aa0589cc17667c655dab638c73d8c87e0e0500b
geslyn <61592906+geslyn@users.noreply.github.com>
2020-04-25 18:52:36 -0400

    Update Test 1.py

--- Test 1.py
+++ Test 1.py
@@ -5,7 +5,7 @@ name = input('Hello! What is your name? \n')
 
 randomNumber = random.randint(1, 50)
 
-guess = int (input("Welcome, %s! , Lets play a game. Please guess a number between 1 and 50: " % (name)))
+guess = int (input("Welcome, %s!, Do you want play a game? :) Please guess a number between 1 and 50: " % (name)))
 
 while randomNumber != guess:
 
