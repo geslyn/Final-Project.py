3212ed7b61b0f23204ed014402292cbfd18e6f75
geslyn <61592906+geslyn@users.noreply.github.com>
2020-04-25 18:53:52 -0400

    Update Test 1.py

--- Test 1.py
+++ Test 1.py
@@ -10,11 +10,11 @@ guess = int (input("Welcome, %s!, Do you want play a game? :) Please guess a num
 while randomNumber != guess:
 
  if guess < randomNumber:
-     print ("Hint: Guess is too low! Try again.")
+     print ("Hint: Your guess is too low! Let's try again.")
      guess = int(input("Guess a number between 1 and 50: "))
     
  elif guess > randomNumber:
-     print ("Hint: Guess is too high! Try again.")
+     print ("Hint: Your guess is too high! Let's try again.")
      guess = int(input("Guess a number between 1 and 50: "))
 
 print('Great Job! You guessed it')
