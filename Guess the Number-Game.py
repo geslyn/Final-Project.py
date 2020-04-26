1b8500ac532f6ffeeafac6cf7bc8d0002615b8d0
geslyn <61592906+geslyn@users.noreply.github.com>
2020-04-25 21:24:48 -0400

    Update Guess the Number-Game.py

--- Guess the Number-Game.py
+++ Guess the Number-Game.py
@@ -3,7 +3,7 @@ import os
 
 name = input('Hello! What is your name? \n')
 
-randomNumber = random.randint(1, 50)
+randomnumber = random.randint(1, 50)
 
 guess = int (input("Welcome, %s!, Do you want play a game? :) Please guess a number between 1 and 50: " % (name)))
 
